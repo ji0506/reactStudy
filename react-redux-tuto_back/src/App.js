e14e56789709308ef073f897668cbfdae0806a11
jiwan <audwldhks0@gmail.com>
2023-08-16 19:13:20 +0900

    react

---
+++ react-redux-tuto_back/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './components/Counter';
+import Todos from './components/Todos';
+import {CounterContainer} from '../containers/CounterContainer';
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
+    </div>
+  );
+};
+export default App;
